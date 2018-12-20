dd71fa813a6dae2ac94a1d6a5d010b4464db19af
captainayan <ayanchakraborty105@gmail.com>
2018-12-20 18:46:44 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ayans_todo
+"# accountant" 
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,116 +1,49 @@
 package main.java.controller;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXListView;
-import com.jfoenix.controls.JFXProgressBar;
-import com.jfoenix.controls.JFXTextField;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import main.java.SQLiteHelper;
-import main.java.models.Task;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.*;
 
-public class Controller implements Initializable {
+public class MainController implements Initializable {
 
     @FXML
-    private JFXListView<String> task_list_view;
-    @FXML
-    private JFXButton add_btn;
-    @FXML
-    private JFXTextField task_name;
+    private Button ui_one;
 
-    private SQLiteHelper mSqLiteHelper;
-    private ObservableList<String> task_list;
-    private ArrayList<Task> tasks_array_list;
+    @FXML
+    private Button ui_two;
+
+    @FXML
+    private Button ui_three;
+
+    @FXML
+    private Button ui_four;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        task_name.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
-                if (task_name.getText().length() > 40) {
-                    String s = task_name.getText().substring(0, 40);
-                    task_name.setText(s);
-                }
-            }
+        // ui_one button click event
+        ui_one.setOnAction((m) -> {
+            System.out.println("Mouse pressed");
         });
 
-        // setting up database
-        mSqLiteHelper = new SQLiteHelper();
-        mSqLiteHelper.connect();
-        mSqLiteHelper.createNewTable();
-
-        // initializing observable list
-        task_list = FXCollections.observableArrayList();
-
-        setListView();
-
-        // item click listener
-        task_list.addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable observable) { /*list changed*/ }
-        });
-        task_list_view.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (!task_list_view.getSelectionModel().getSelectedIndices().isEmpty()) {
-                    tasks_array_list.get(task_list_view.getSelectionModel().getSelectedIndices().get(0)).remove(mSqLiteHelper);
-                    setListView();
-                }
-            }
+        // ui_one button click event
+        ui_two.setOnAction((m) -> {
+            System.out.println("Mouse pressed");
         });
 
-    }
-
-
-    // onclick add button
-    @FXML
-    void addData(ActionEvent event) {
-        String task = task_name.getText();
-        if (!task.equals("")) {
-            Task.save(task, mSqLiteHelper);
-
-            setListView();
-
-            task_name.setText("");
-        }
-    }
-
-    private void setListView() {
-
-        try {
-            task_list.clear();
-            tasks_array_list.clear();
-        }
-        catch (Exception e) {}
-
-        // getting all tasks in an array list
-        tasks_array_list = Task.getAllData(mSqLiteHelper);
-
-        // assigning data from array list to observable list
-        tasks_array_list.forEach((task) -> {
-            task_list.add(task.getTask());
+        // ui_one button click event
+        ui_three.setOnAction((m) -> {
+            System.out.println("Mouse pressed");
         });
 
-        // setting observable list view
-        task_list_view.setItems(task_list);
+        // ui_one button click event
+        ui_four.setOnAction((m) -> {
+            System.out.println("Mouse pressed");
+        });
+
     }
 
 }
